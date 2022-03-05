808b70e8bd327c443eb58067e5c3218200c4df61
lovelaught <darya.bratova13@gmail.com>
2022-03-05 21:21:18 +0300

    add font & styled-reset

--- src/App/App.styles.tsx
+++ src/App/App.styles.tsx
@@ -1,5 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
+import "@fontsource/roboto";
 
 // eslint-disable-next-line import/prefer-default-export
 export const Global = createGlobalStyle`
